99266f479d96a1af9e772bfed5441a3b2e2b51b8
Pingw1 <79995382+Pingw157@users.noreply.github.com>
2022-12-19 20:39:18 +0100

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -6,7 +6,7 @@ var preLoad = async function() {
   console.log('[Service Worker] Install Event processing');
   let cache = await caches.open('pwabuilder-offline');
   console.log('[Service Worker] Cached index and offline page during Install');
-  return cache.addAll(['/index.html', '/gamestyle.css', '/manifest.json', '/js/gamevsIA.js', '/js/sb-pwa.js']);
+  return cache.addAll(['/index.html', '/gamestyle.css', '/manifest.json', '/js/', '/js/gamevsIA.js', '/js/sb-pwa.js']);
 }
 
 self.addEventListener('fetch', function(event) {
