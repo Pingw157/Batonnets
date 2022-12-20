8a800074c0dfaeb60c5b507bc00d1f18024a3fc7
Pingw1 <79995382+Pingw157@users.noreply.github.com>
2022-12-20 13:16:48 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('fox-store').then((cache) => cache.addAll(['/index.html', '/gamestyle.css', '/manifest.json', '/app.js']))
+    caches.open('game-store').then((cache) => cache.addAll(['/index.html', '/gamestyle.css', '/manifest.json', '/app.js']))
   );
 });
 
@@ -9,4 +9,30 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request)),
   );
+});
+
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('game-store').then(async (cache) => {
+    let ok,
+    c = ['/index.html', '/gamestyle.css', '/manifest.json', '/app.js'];
+
+    console.log('ServiceWorker: Caching files:', c.length, c);
+    try {
+      ok = await cache.addAll(c);
+    } catch (err) {
+      console.error('sw: cache.addAll');
+      for await (let i of c) {
+        try {
+          ok = await cache.add(i);
+        } catch (err) {
+          console.warn('sw: cache.add',i);
+        }
+      }
+    }
+
+    return ok;
+  }));
+
+  console.log('ServiceWorker installed');
 });
