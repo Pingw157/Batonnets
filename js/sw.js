17d99d52fa85647c1e8fad80f1621e7e2bb75061
Pingw1 <79995382+Pingw157@users.noreply.github.com>
2022-12-20 10:03:01 +0100

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,14 +1,11 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(preLoad());
-});
-
-var preLoad = async function() {
-  console.log('[Service Worker] Install Event processing');
-  let cache = await caches.open('pwabuilder-offline');
-  console.log('[Service Worker] Cached index and offline page during Install');
-  return cache.addAll(['/index.html', '/gamestyle.css', '/manifest.json', '/js/', '/js/gamevsIA.js', '/js/sb-pwa.js']);
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll(['/index.html', '/gamestyle.css', '/manifest.json', '/js/', '/js/gamevsIA.js', '/js/sb-pwa.js']);
 }
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
 });
