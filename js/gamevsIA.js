8162074108e21e438af39893adcaab931c105305
Pingw1 <79995382+Pingw157@users.noreply.github.com>
2022-12-20 11:09:11 +0100

    Update gamevsIA.js

--- js/gamevsIA.js
+++ js/gamevsIA.js
@@ -1,3 +1,8 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./sw.js')
+    .then(() => { console.log('Service Worker Registered'); });
+}
 var userName = window.prompt('Enter your user name');
 if (!userName) {
   userName = "Nameless player";
